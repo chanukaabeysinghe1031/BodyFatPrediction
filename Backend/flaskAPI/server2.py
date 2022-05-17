81189c0b16d46761bc5c0393c87a66a74ec07786
Chanuka Abeysinghe <abeysinghechanuka@gmail.com>
2022-05-17 10:29:30 +0530

    Connected NodeJs API and Flask API

--- Backend/flaskAPI/server2.py
+++ Backend/flaskAPI/server2.py
@@ -1,6 +1,6 @@
 from flask import Flask , jsonify , request
 from keras.models import load_model
-
+import base64
 app = Flask(__name__)
 
 def getModel():
@@ -16,14 +16,24 @@ def index():
     return jsonify({"API":"This is the python API for getting the predictions from the models"})
 
 @app.route('/predictBodyFat',methods=['POST'])
-def getBodyPrediction():
+def getBodyFatPrediction():
     data = request.get_json(force=True)
     encoded = data['modelInput']
     prediction = model.predict(encoded).tolist()
     response = {
         'prediction': prediction[0]
     }
-    return jsonify({"You Sent": response}), 201
+    return jsonify(response), 201
 
+@app.route('/predictBodyFat',methods=['POST'])
+def getMoodPrediction():
+    data = request.get_json(force=True)
+    encoded = data['image']
+    decoded = base64.b64decode(encoded)
+    prediction = model.predict(encoded).tolist()
+    response = {
+        'prediction': prediction[0]
+    }
+    return jsonify({"You Sent": response}), 201
 if __name__ == "__main__":
     app.run(debug=True)
--- Backend/nodeJSAPI/api/controllers/bodyFatPrediction_controller.js
+++ Backend/nodeJSAPI/api/controllers/bodyFatPrediction_controller.js
@@ -1,5 +1,7 @@
 const UserFatLevelRecords = require('../models/userFatLevelRecord')
+const request = require('request-promise')
 
+PYTHON_FLASK_API_URL = "http://127.0.0.1:5000/"
 exports.predictBodyFatLevel = (req,res) => {
     const {
         userId,age, weight,height,neck,chest,abdomen,hip,knee,thigh,ankle,biceps,forearm,wrist
@@ -11,25 +13,46 @@ exports.predictBodyFatLevel = (req,res) => {
             res.json({Status: "Unsuccessful", Message: "All the data must be entered."})
     }else{
         // Take the prediction for the body fat level from the flask api
-        let fatLevelPrediction = 44.44;
-        const newRecord  = new UserFatLevelRecords({
-            userId,age, weight,height,neck,chest,abdomen,hip,knee,thigh,ankle,biceps,forearm,wrist,fatLevelPrediction
-        })
-        newRecord.save()
-        .then(result=>{
-            res.json({
-                Status: "Successful",
-                Message: 'Record  has been saved successfully.',
-                Prediction : fatLevelPrediction
+        const options = {
+            method: 'POST',
+            uri: PYTHON_FLASK_API_URL+'predictBodyFat',
+            body: {
+                "modelInput":[[23,154.25,67.75,36.2,93.1,85.2,94.5,59.0,37.3,21.9,32.0,27.4,17.1]]
+            },
+            json: true,
+            headers: {
+                'Content-Type': 'application/json',
+            }
+        }
+        request(options).then(function (response){
+            let dataFromFlaskAPI = JSON.stringify(response)
+            dataFromFlaskAPI= JSON.parse(dataFromFlaskAPI)
+            dataFromFlaskAPI = dataFromFlaskAPI.prediction
+            let fatLevelPrediction = dataFromFlaskAPI[0];
+            const newRecord  = new UserFatLevelRecords({
+                userId,age, weight,height,neck,chest,abdomen,hip,knee,thigh,ankle,biceps,forearm,wrist,fatLevelPrediction
+            })
+            newRecord.save()
+            .then(result=>{
+                res.json({
+                    Status: "Successful",
+                    Message: 'Record  has been saved successfully.',
+                    Prediction : fatLevelPrediction
+                })
+            })
+            .catch(error=>{
+                res.json({
+                    Status: "Unsuccessful",
+                    Message: "Happened while saving the body fat record in the DB.",
+                    error: error
+                })
             })
         })
-        .catch(error=>{
-            console.log(error)
+        .catch(function (err) {
             res.json({
                 Status: "Unsuccessful",
-                Message: "Happened saving the record in " +
-                    "DB.",
-                error: error
+                Message: "Happened while sending the request to Flask API",
+                error: err
             })
         })
     }
